bbb838a615bd1f6a88b206e4f899aa60177471df
Rohan Verma <sourcecode@gvmtechnologies.com>
2022-06-07 21:14:40 +0530

    logout fixed

--- src/components/logout-buttons/GoogleLogout.js
+++ src/components/logout-buttons/GoogleLogout.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Button } from "reactstrap";
-import { useGoogleLogout } from "react-google-login";
+// import { useGoogleLogout } from "react-google-login";
 import { useHistory } from "react-router-dom";
 
 const GoogleLogout = () => {
   const history = useHistory();
-  const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  // const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
+  // const onLogoutSuccess = (res) => {
+  //   localStorage.removeItem("token");
+  //   sessionStorage.removeItem("fbssls_442121237680668");
+  //   console.log("Logged out success");
+  //   alert("Logged out successfully");
+  //   history.push("/argon/login-page");
+  // };
+  // const onFailure = (res) => {
+  //   console.log("Logout Failed", res);
+  // };
+  // const { signOut } = useGoogleLogout({
+  //   onLogoutSuccess,
+  //   onFailure,
+  //   clientId,
+  // });
+  
   const onLogoutSuccess = (res) => {
     localStorage.removeItem("token");
     sessionStorage.removeItem("fbssls_442121237680668");
@@ -14,19 +30,11 @@ const GoogleLogout = () => {
     alert("Logged out successfully");
     history.push("/argon/login-page");
   };
-  const onFailure = (res) => {
-    console.log("Logout Failed", res);
-  };
-  const { signOut } = useGoogleLogout({
-    onLogoutSuccess,
-    onFailure,
-    clientId,
-  });
   return (
     <Button
       className="btn-neutral btn-icon"
       color="default"
-      onClick={signOut}
+      onClick={onLogoutSuccess}
     >
       <span className="btn-inner--text">Logout</span>
     </Button>
